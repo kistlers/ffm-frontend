eadf8c22c90ec21e772065f59cb7c3a924619581
Severin Kistler <kistler@ubique.ch>
2020-07-28 16:14:20 +0200

    skeleton with routes working

--- fronis-frontend/src/app/app-routing.module.ts
+++ fronis-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
+import { TeamComponent } from "./team/team.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "home", component: HomeComponent},
+  {path: "team", component: TeamComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
