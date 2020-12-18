c2018a00c3f67b5345d9bb0422ac9138e80b715e
Severin Kistler <kistler@ubique.ch>
2020-12-18 22:05:33 +0100

    both: fix remaining references to fronis

--- ffm-web/src/app/app.component.spec.ts
+++ ffm-web/src/app/app.component.spec.ts
@@ -20,7 +20,7 @@ describe("AppComponent", () => {
         expect(app).toBeTruthy();
     });
 
-    it(`should have as title 'fronis-web'`, () => {
+    it(`should have as title 'ffm-web'`, () => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.componentInstance;
         expect(app.title).toEqual("ffm-web");
